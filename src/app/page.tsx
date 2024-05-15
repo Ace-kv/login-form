ed87b76198539c215066a93d2954e355d15dcadc
Ace-kv <talhashahid100@yahoo.com>
2024-05-15 07:46:17 +0500

    fine-tuning responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Page() {
     <section className="flex justify-center items-center h-screen py-28">
         <div className="flex flex-col justify-center max-w-3xl w-full mx-auto px-4">
             <div className="flex justify-center">
-                <div className="login-headings w-3/4 sm:w-1/2 mb-12">
+                <div className="login-headings w-5/6 sm:w-1/2 mb-12">
                     <h2 className="px-4">Login</h2>
                 </div>
             </div>
             <div className="flex justify-center">
-                <div className="w-3/4 sm:w-1/2">
+                <div className="w-5/6 sm:w-1/2">
                     <div className="flex flex-col justify-center">                   
                         <div className="login-headings font-thin">
                             <h4 className="px-4 px">Have an Account?</h4>
@@ -22,8 +22,8 @@ export default function Page() {
                         <div className="flex flex-col justify-center text-center">
                             <p className="mb-1 text-center">— Or Sign In With —</p>
                             <div className="flex">
-                                <Button value="Facebook" url="#" px="px-10 sm:px-12 ml-2"/>
-                                <Button value="Twitter" url="#" px="px-12 sm:px-14 mr-1"/>
+                                <Button value="Facebook" url="#" px="px-8 sm:px-12 ml-2"/>
+                                <Button value="Twitter" url="#" px="px-10 sm:px-14 mr-1"/>
                             </div>
                         </div>
                     </div>
